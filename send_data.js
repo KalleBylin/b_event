d11c513194cf408cc65dc634fcf3a9011426d728
Kalle Bylin <kallebylin@hotmail.com>
2020-11-24 21:56:56 -0500

    Create send_data.js

---
+++ send_data.js
@@ -0,0 +1,14 @@
+function sendData(event_data, endpoint) {
+  var xhr = new XMLHttpRequest();
+  var stringifiedData = JSON.stringify(event_data);
+
+  xhr.open('POST', endpoint);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(stringifiedData);
+
+  xhr.onload = function () {
+    if (xhr.status.toString()[0] !== '2') {
+      console.error(xhr.status + '> ' + xhr.statusText);
+    }
+  };
+}
